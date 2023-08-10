02ad91ae8acace17c7915fa911977dd980cb5c9e
Nandier Ruiz <nandier.ruiz@gmail.com>
2023-08-10 11:27:11 -0500

    feat: :sparkles: Router initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { MainProvider } from './context'
 import App from './App'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MainProvider>
-      <App />
-    </MainProvider>
+    <BrowserRouter>
+      <MainProvider>
+        <App />
+      </MainProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
